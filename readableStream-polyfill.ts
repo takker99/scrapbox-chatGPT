344868e73a2364cef673ab49f82dbd5a5a4e4284
takker99 <37929109+takker99@users.noreply.github.com>
2023-05-07 06:28:24 +0900

    :rotating_light:

--- readableStream-polyfill.ts
+++ readableStream-polyfill.ts
@@ -1,4 +1,5 @@
 // ported from https://bugs.chromium.org/p/chromium/issues/detail?id=929585#c10
+// @ts-ignore 型定義とは食い違うmethodをpolyfillで挿入する
 if (typeof ReadableStream.prototype[Symbol.asyncIterator] !== "function") {
   Object.defineProperty(ReadableStream.prototype, Symbol.asyncIterator, {
     async *value() {
