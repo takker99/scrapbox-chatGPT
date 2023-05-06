6d0e2155ed5ce3c9915754bbe09312183fc1dcb7
takker99 <37929109+takker99@users.noreply.github.com>
2023-05-07 07:41:42 +0900

    :recycle: shorten magic characters

--- uuid.ts
+++ uuid.ts
@@ -1,23 +1,6 @@
 /** create UUID for chatGPT */
 export const uuid = (): string => {
-  const chars = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "0",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-  ];
+  const chars = "abcdef0123456789";
   const selected: string[] = [];
   for (let n = 0; n < 36; n++) {
     selected[n] = n === 8 || n === 13 || n === 18 || n === 23
