ebdcdb0baed065f1c0680419861175fc4653ee6b
takker99 <37929109+takker99@users.noreply.github.com>
2023-05-07 06:21:23 +0900

    :sparkles: Get existed conservations

---
+++ getConversation.ts
@@ -0,0 +1,72 @@
+import {
+  BlockedByCloudflareError,
+  makeTooManyRequestsError,
+  makeUnauthorizedError,
+  Result,
+  TooManyRequestsError,
+  UnauthorizedError,
+} from "./errors.ts";
+import { getAccessToken, resetAccessToken } from "./accessToken.ts";
+import { Prompt } from "./types.ts";
+
+declare const GM_fetch: (typeof fetch) | undefined;
+
+export interface Conversation {
+  title: string;
+  /** 会話を始めた日時 (UNIX TIME) */
+  create_time: number;
+  /** 最後に会話した日時 (UNIX TIME) */
+  update_time: number;
+  mapping: Record<string, Message>;
+  moderation_results: [];
+  /** 一番最後の会話のID */
+  current_node: string;
+}
+
+export interface Message {
+  id: string;
+  message?: Prompt;
+  parent?: string;
+  children: string[];
+}
+/** get an existed conversation */
+export const getConversation = (conversationId: string):
+  | Promise<
+    Result<
+      Conversation,
+      UnauthorizedError | BlockedByCloudflareError | TooManyRequestsError
+    >
+  >
+  | undefined => {
+  if (!GM_fetch) return;
+
+  const result = getAccessToken();
+  if (!result) return;
+  return (async () => {
+    const tokenRes = await result;
+    if (!tokenRes.ok) return tokenRes;
+
+    const res = await GM_fetch(
+      `https://chat.openai.com/backend-api/conversation/${conversationId}`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${tokenRes.value}`,
+          Referrer: "https://chat.openai.com",
+        },
+      },
+    );
+    if (!res.ok) {
+      switch (res.status) {
+        case 401:
+          resetAccessToken();
+          return makeUnauthorizedError();
+        case 429:
+          return makeTooManyRequestsError();
+      }
+    }
+
+    const value = await res.json() as Conversation;
+    return { ok: true, value };
+  })();
+};
