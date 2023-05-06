b2764597cb3d21c029781c57d5080b9a1f890445
takker99 <37929109+takker99@users.noreply.github.com>
2023-05-07 06:20:40 +0900

    :sparkles: Enable to get an access token

---
+++ accessToken.ts
@@ -0,0 +1,67 @@
+import {
+  BlockedByCloudflareError,
+  isBlockedByCloudflare,
+  makeBlockedByCloudflareError,
+  makeUnauthorizedError,
+  Result,
+  UnauthorizedError,
+} from "./errors.ts";
+
+declare const GM_fetch: (typeof fetch) | undefined;
+
+let accessToken = "";
+let tokenExpires = 0;
+
+export interface ChatGPTSession {
+  user: {
+    id: string;
+    name: string;
+    email: string;
+    image: string;
+    picture: string;
+    mfa: boolean;
+    groups: unknown[];
+    intercom_hash: string;
+  };
+  /** ISO8601形式 */
+  expires: string;
+  accessToken: string;
+  authProvider: string;
+}
+
+/** chatGPTのaccess tokenを取得する
+ *
+ * 一度取得したものをcacheに保持し、有効期限が切れるまでそれを返す
+ * @return 成功時はaccess token、失敗時はエラー情報を返す
+ */
+export const getAccessToken = ():
+  | Promise<
+    Result<string, UnauthorizedError | BlockedByCloudflareError>
+  >
+  | undefined => {
+  if (accessToken && tokenExpires > new Date().getTime()) {
+    return Promise.resolve({
+      ok: true,
+      value: accessToken,
+    });
+  }
+
+  if (!GM_fetch) return;
+
+  return (async () => {
+    const res = await GM_fetch("https://chat.openai.com/api/auth/session");
+    const text = await res.text();
+
+    if (isBlockedByCloudflare(text)) return makeBlockedByCloudflareError();
+    const json = JSON.parse(text);
+    if (!json.token) return makeUnauthorizedError();
+    const { accessToken: token, expires } = json as ChatGPTSession;
+    if (expires) tokenExpires = new Date(expires).getTime();
+    accessToken = token;
+    return { ok: true, value: token };
+  })();
+};
+
+export const resetAccessToken = (): void => {
+  accessToken = "";
+};
